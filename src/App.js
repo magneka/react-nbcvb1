8c67ef8e6d24bc3910c451d2f18936f3640c24a4
magneka <magne@alvheim.com>
2020-11-05 18:20:35 +0100

    useAxios laget

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios"
+import { useAxios } from './useAxios'
 import "./style.css";
 
+// Click project og du ser git knappen
+
 export default function App() {
+
+  const {users, getData} = useAxios()
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <h1>Hello Ulriken consulting!</h1>
       <p>Start editing to see some magic happen :)</p>
+
+    <button onClick={getData}>
+      Klikk på meg
+    </button>
+
+    <div>
+      <ul>
+        {users.map((item, i) => <li>{item.lastname}, {item.firstname}</li>)}
+      </ul>
+    </div>
+
     </div>
   );
 }
---
+++ src/useAxios.js
@@ -0,0 +1,23 @@
+  
+import { useState } from "react";
+import axios from "axios"
+
+export const useAxios = () => {
+
+  const [users, setUsers] = useState([])
+
+  const getData = () => {      
+    axios.get(
+      `https://replitExpress.magneka.repl.co/users2`
+    )
+    .then(result => {
+      console.log(result);  
+      setUsers(result.data)      
+    })
+    .catch(error => {
+      console.error("error: ", error);        
+    });
+  } 
+
+  return { users, getData }
+}
